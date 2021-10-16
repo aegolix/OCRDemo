c95763049248f1c5f7c119adb830177b8a9a7fae
aegolix <strigix8@gmail.com>
2021-10-16 19:33:18 +0700

    upload project

---
+++ app/src/main/java/com/sydien/ocrdemo/Configs.java
@@ -0,0 +1,11 @@
+package com.sydien.ocrdemo;
+
+import android.util.SparseIntArray;
+import android.view.Surface;
+
+public class Configs {
+    public static String WARN_NO_CAMERA_FOUND = "Failed to obtain camera! App closes";
+    public static String TESS_NO_TEXT = "Không tìm thấy văn bản";
+    public static String TESS_PATH = "OCRDemo/tesseract/";
+    public static String TESS_FILE = "eng.traineddata";
+}
